2b69960317b9c5fffed6e2b4421d8561169ca78b
JoseIgnacioTapia <josignaciot@gmail.com>
2023-06-04 19:40:04 -0300

    Header

---
+++ app/components/Header.tsx
@@ -0,0 +1,17 @@
+import Search from './ui/Search';
+
+function Header(): JSX.Element {
+  return (
+    <header>
+      <div>
+        <div>
+          <p></p>
+          <Search />
+        </div>
+        <div></div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ app/components/ui/Search.tsx
@@ -0,0 +1,10 @@
+function Search(): JSX.Element {
+  return (
+    <form>
+      <input type="text" />
+      <button type="submit">Buscar</button>
+    </form>
+  );
+}
+
+export default Search;
