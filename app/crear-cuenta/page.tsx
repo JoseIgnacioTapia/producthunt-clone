8bb2ad234865111da033774de0ded30b6c9f7732
JoeIt <josignaciot@gmail.com>
2023-06-05 17:31:40 -0300

    Create the Account Creation Form

--- app/crear-cuenta/page.tsx
+++ app/crear-cuenta/page.tsx
@@ -1,5 +1,32 @@
 function CreateAccout(): JSX.Element {
-  return <div>Create Account</div>;
+  return (
+    <>
+      <h1>Create Account</h1>
+      <form>
+        <div>
+          <label htmlFor="name">Nombre</label>
+          <input type="text" id="name" placeholder="Tu Nombre" name="nombre" />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Tu Email" name="email" />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Tu plassword"
+            name="password"
+          />
+        </div>
+
+        <input type="submit" value="Crear Cuenta" />
+      </form>
+    </>
+  );
 }
 
 export default CreateAccout;
