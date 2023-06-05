6b6e7830ed72dd62a29c0943c1c33378d1118967
JoeIt <josignaciot@gmail.com>
2023-06-05 11:39:43 -0300

    Header Styles

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,36 @@
-import Link from 'next/link';
-import Navegation from './Navegation';
-import Search from './ui/Search';
+"use client";
+import Link from "next/link";
+import Navegation from "./Navegation";
+import Search from "./ui/Search";
+import styled from "styled-components";
+
+const ContenedorHeader = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Logo = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: var(--font-roboto), serif;
+  margin-right: 2rem;
+`;
 
 function Header(): JSX.Element {
   return (
-    <header>
-      <div>
+    <header style={{ borderBottom: "2px solid #e1e1e1", padding: "1rem 0" }}>
+      <ContenedorHeader>
         <div>
-          <p></p>
+          <Link href="/">
+            <Logo>P</Logo>
+          </Link>
           <Search />
           <Navegation />
         </div>
@@ -18,7 +41,7 @@ function Header(): JSX.Element {
           <Link href="/">Login</Link>
           <Link href="/">Crear cuenta</Link>
         </div>
-      </div>
+      </ContenedorHeader>
     </header>
   );
 }
